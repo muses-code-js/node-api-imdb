9f02006c5aac2d035593470049a7125138cb40e8
ticidesign <ticianadeandrade@Ticianas-MacBook-Pro.local>
2018-08-30 19:08:31 +1000

    Create DB test

---
+++ src/database/index.spec.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/node-api-imdb', { useNewUrlParser: true });
+
+const database = require('./index');
+
+describe('database', () => {
+	beforeEach(() => mongoose.connection.collections['movies'].drop());
+	
+  test('save movies', async () => {
+    const movie = {
+      title: 'Star Wars',
+      description: 'A long time ago in a galaxy far far away...'
+    }
+		const savedMovie = await database.save(movie);
+		const movies = await database.getAll();
+
+		expect(savedMovie.id).toBeDefined();
+		expect(movies[0].title).toBe(movie.title);
+	});
+});
